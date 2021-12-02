2577c8dcc6dcd44218f0d58af8492063c8f00a06
João Felipe P <jf.pelliccione@gmail.com>
2021-12-02 09:27:35 -0300

    Inserção do background e do logo.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import SearchBoxFilter from './SearchBoxFilter';
+import logo from '../images/star-wars-logo.png'
 
 function Header() {
   return (
     <header id="pageHeader">
       <div id="headerCentralContainer">
-        {/* <img
-          src={}
+        <img
+          src={logo}
           alt="Logo Star Wars"
-        /> */}
+        />
         <SearchBoxFilter />
       </div>
     </header>
